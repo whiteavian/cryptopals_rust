cf88b8ef4afd70e97232712663aae5be11a65391
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-10-04 18:21:03 -0400

    Add xor for set1ch2 (pairing with lpesk and ameliariely)

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,34 @@ const SET1CH1_TEST_OUT: &str =
 
 fn main() {
     set1ch1();
+    set1ch2();
 }
 
 fn set1ch1() {
     let b64 = hex_to_base64(SET1CH1_TEST_IN);
     assert_eq!(b64, SET1CH1_TEST_OUT);
     println!("{:?}", b64);
-    println!("Success!");
+    println!("Success on Set 1 Challenge 1!");
 }
 
 fn hex_to_base64(hex: &str) -> String {
     let bytes = HEXLOWER.decode(hex.as_bytes()).unwrap();
     BASE64.encode(&bytes)
+}
+
+fn set1ch2() {
+    assert_eq!(xor(hex_to_bytes("1c0111001f010100061a024b53535009181c"), hex_to_bytes("686974207468652062756c6c277320657965")), hex_to_bytes("746865206b696420646f6e277420706c6179"));
+    println!("Success on Set 1 Challenge 2!");
+}
+
+fn xor(vec1 : Vec<u8>, vec2 : Vec<u8>) -> Vec<u8> {
+    assert_eq!(vec1.len(), vec2.len());
+    vec1.iter()
+        .zip(vec2.iter())
+        .map(|(x , y)| x ^ y)
+        .collect()
+}
+
+fn hex_to_bytes(hex: &str) -> Vec<u8> {
+    HEXLOWER.decode(hex.as_bytes()).unwrap()
 }
