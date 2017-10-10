fea784015e0adb079ead4e6df81a211621bed6c0
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-10-10 16:47:42 -0400

    Move expected onto its own line.

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,8 @@ I go crazy when I hear a cymbal";
     let key = "ICE";
     let result = repeat_xor_encrypt(input, key);
 
-    let expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+    let expected =
+        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
 
     assert_eq!(result, hex_to_bytes(expected));
     println!("Success on Set 1 Challenge 5!");
